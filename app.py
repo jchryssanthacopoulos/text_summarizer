174abc4281716eca1a606eff0266d80c5d8ad8f5
James Chryssanthacopoulos <jchryssanthacopoulos@gmail.com>
2023-02-22 18:50:50 +0100

    Extend to handle different prompts

--- app.py
+++ app.py
@@ -5,19 +5,22 @@ from text_summarizer.functions import summarize
 
 
 try:
-  openai.api_key = os.getenv('OPENAI_KEY')
-  
-  if "summary" not in st.session_state:
-      st.session_state["summary"] = ""
-  
-  st.title("Text Summarizer")
-  
-  input_text = st.text_area(label="Enter full text:", value="", height=250)
-  st.button(
-      "Submit",
-      on_click=summarize,
-      kwargs={"prompt": input_text},
-  )
-  output_text = st.text_area(label="Summarized text:", value=st.session_state["summary"], height=250)
+    openai.api_key = os.getenv('OPENAI_KEY')
+
+    if "summary" not in st.session_state:
+        st.session_state["summary"] = ""
+
+    st.title("Text Analyzer")
+
+    task_type = st.text_area(label="Enter task type (summary or rating)", value="", height=20)
+
+    input_text = st.text_area(label="Enter full text:", value="", height=250)
+    st.button(
+        "Submit",
+        on_click=summarize,
+        kwargs={"text": input_text, "task": task_type},
+    )
+
+    output_text = st.text_area(label="Analyzed text:", value=st.session_state["summary"], height=250)
 except:
-  st.write('There was an error =(')
+    st.write('There was an error =(')
--- text_summarizer/functions.py
+++ text_summarizer/functions.py
@@ -2,9 +2,23 @@ import openai
 import streamlit as st
 
 
-def summarize(prompt):
-    augmented_prompt = f"summarize this text: {prompt}"
+def summarize(task, text):
     try:
+        if task == "summary":
+            augmented_prompt = (
+                f"Below is a conversation between a client looking to rent an apartment, and a leasing agent helping to "
+                "answer their questions. Can you provide a summary of the conversation?\n\n"
+                f"{text}"
+            )
+        elif task == "rating":
+            augmented_prompt = (
+                f"Below is a conversation between a client looking to rent an apartment, and a leasing agent helping to "
+                "answer their questions. Can you rate the quality of the agent's responses overall on a scale of 1 to 5?\n\n"
+                f"{text}"
+            )
+        else:
+            raise Exception("Invalid task type")
+
         st.session_state["summary"] = openai.Completion.create(
             model="text-davinci-003",
             prompt=augmented_prompt,
